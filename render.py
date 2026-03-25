915072b7d490082d5ef08965700f55811ec3a16a
MekCraftLi <Jiahao.Mek.Li@outlook.com>
2026-03-26 04:18:32 +0800

    fix: 修复render.py

--- render.py
+++ render.py
@@ -44,8 +44,10 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
             rendering = rendering[..., rendering.shape[-1] // 2:]
             gt = gt[..., gt.shape[-1] // 2:]
 
-        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+            # 使用真实的相机名字来保存图片，以便和掩码对齐
+            file_name = view.image_name + ".png"
+            torchvision.utils.save_image(rendering, os.path.join(render_path, file_name))
+            torchvision.utils.save_image(gt, os.path.join(gts_path, file_name))
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, separate_sh: bool):
     with torch.no_grad():
